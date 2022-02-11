54475afdeade8418a541063e5027b9125452826c
Kanjamad Bosel <npor25@yahoo.com>
2022-02-11 07:09:41 -0800

    Display Tours

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>ours tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          // {...tour} mean that I'll have access to all the properties that are in my object
+          return <Tour key={tour.id} {...tour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
