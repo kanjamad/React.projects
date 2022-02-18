e9e793fa04372135a9bda3f15f68bd6926dbec10
Kanjamad Bosel <npor25@yahoo.com>
2022-02-18 13:42:02 -0800

    Grocery Bud Alert

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -3,7 +3,57 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditId] = useState(null)
+  const [alert, setAlert] = useState({
+    show: false,
+    msg: '',
+    type: '',
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!name) {
+      // display alert
+      setAlert({ show: true, msg: 'please enter value', type: 'danger' })
+    } else if (name && isEditing) {
+      // deal with Edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name }
+      setList([...list, newItem])
+      setName('')
+    }
+  }
+
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} />
+          <button className='clear-btn'>clear items</button>
+        </div>
+      )}
+    </section>
+  )
 }
 
 export default App
--- README.md
+++ README.md
@@ -20,7 +20,7 @@
 
 8. Lorem Ipsum Generator [Project Site](https://react-projects-8-lorem-ipsum-generator.netlify.app/)
 9. Color Shades Generator [Project Site](https://react-projects-9-color-generator.netlify.app/)
-10. Grocery Bud
+10. Grocery Bud [Project Site](https://react-projects-10-grocery-bud.netlify.app/)
 
 #### useRef
 
